9e864268e442eb0319655c10cb12b54fb07e7a0a
amiam <amiambagheri@gmail.com>
2024-10-06 10:55:59 +0330

    feat: add simple inventory management system project

---
+++ Inventory Management/Program.cs
@@ -0,0 +1,16 @@
+﻿using Inventory_Management;
+
+List<Product<object>> inventory =
+[
+    new Product<object>("Laptop", ProductsCategory.Electronics, new ElectronicDetails("Dell", 899.99)),
+            new Product<object>("T-Shirt", ProductsCategory.Clothing, new ClothingDetails("Medium", "Cotton")),
+            new Product<object>("Book", ProductsCategory.Books, "Introduction to C# Programming"),
+            new Product<object>("Apple", ProductsCategory.Food, "Red Delicious")
+];
+
+foreach (var product in inventory)
+{
+    Console.WriteLine("---------");
+    product.DisplayInfo();
+    Console.WriteLine("---------");
+}
